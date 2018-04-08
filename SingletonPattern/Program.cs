1d5dff9a413ef16806930f3d48ca701cb524b73f
unknown <ivanp@dump.hr>
2018-04-08 21:22:37 +0200

    Added Singleton Pattern

---
+++ SingletonPattern/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace SingletonPattern
+{
+    /// <summary>
+    /// 
+    /// The Singleton Pattern ensures a class has only one instance,
+    /// and provides a global point of access to it.
+    /// 
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ SingletonPattern/Singleton.cs
@@ -0,0 +1,45 @@
+﻿namespace SingletonPattern
+{
+    /// <summary>
+    /// 
+    /// Not a thread safe implementation.
+    /// For alternative versions: http://csharpindepth.com/Articles/General/Singleton.aspx 
+    /// 
+    /// </summary>
+    public class Singleton
+    {
+        private static Singleton uniqueInstance;
+
+        private Singleton()
+        {
+
+        }
+
+        public static Singleton GetInstance()
+        {
+            if (uniqueInstance == null)
+            {
+                uniqueInstance = new Singleton();
+            }
+
+            return uniqueInstance;
+        }
+
+        // other useful methods here
+    }
+
+
+    // fully lazily-loaded, thread-safe, simple and highly performant version
+
+    //public sealed class Singleton
+    //{
+    //    private static readonly Lazy<Singleton> lazy =
+    //        new Lazy<Singleton>(() => new Singleton());
+
+    //    public static Singleton Instance { get { return lazy.Value; } }
+
+    //    private Singleton()
+    //    {
+    //    }
+    //}
+}
